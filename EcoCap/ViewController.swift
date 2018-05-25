87e4482361e80f88ee566bf3e1d7a4c3ea8478dc
renanbronchart <renan.bronchart@gmail.com>
2018-05-25 22:37:57 +0200

    Initial Commit

---
+++ EcoCap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EcoCap
+//
+//  Created by Renan Bronchart on 25/05/2018.
+//  Copyright © 2018 Renan Bronchart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
