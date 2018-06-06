87fbcb578b794745e493fdeb989086c0d5b7b643
renanbronchart <renan.bronchart@gmail.com>
2018-06-06 17:39:07 +0200

    [Feat:profile] Add level bullet on profile

--- EcoCap/SupportFiles/overrideUIImage.swift
+++ EcoCap/SupportFiles/overrideUIImage.swift
@@ -14,30 +14,4 @@ extension UIImageView {
     open override func draw(_ rect: CGRect) {
         self.clipsToBounds = true
     }
-    
-    @IBInspectable
-    var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            self.clipsToBounds = true
-            layer.cornerRadius = newValue
-        }
-    }
-    
-    @IBInspectable
-    var isRounded: Bool {
-        get {
-            return self.isRounded
-        }
-        set {
-            if newValue == true {
-                self.clipsToBounds = true
-                layer.cornerRadius = self.frame.height / 2
-            } else {
-                layer.cornerRadius = 0
-            }
-        }
-    }
 }
